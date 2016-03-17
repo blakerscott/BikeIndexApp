a5506437242a2dd938deee4558437a56019f4cf1
Blake Scott <blakerscott@gmail.com>
2016-03-17 14:49:56 -0700

    add code to interface file to make search results print to page as list items

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -6,8 +6,18 @@ $(document).ready(function() {
     $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + zipCode + '&proximity_square=10', function(response) {
       for(var i = 0; i < response.bikes.length; i++) {
         $('.showBikes').append("The bikes in " + zipCode + " in " + response.bikes[i].title);
-
+        $('.showBikes').append('<ul><li>'+ response.bikes[i].stolen_location + '</li></ul>');
       }
     });
   });
 });
+
+var map;
+var geocoder;
+function initMap() {
+  geocoder = new google.maps.Geocoder();
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: {lat: 45.5207260, lng: -122.6796520}
+  });
+}
