00492c859218781e97b518842f8a273617cd1776
Blake Scott <blakerscott@gmail.com>
2016-03-17 12:02:57 -0700

    add a for each loop browser interface to iterate through bikes in the json array

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
+  $('#bikeLocation').click(function() {
+    var zipCode = $('#location').val();
+    $('#location').val("");
 
+    $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + zipCode + '&proximity_square=10', function(response) {
+      for(var i = 0; i < response.bikes.length; i++) {
+        $('.showBikes').append("The bikes in " + zipCode + " in " + response.bikes[i].title);
+
+      }
+    });
+  });
+});
