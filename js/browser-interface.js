f15c16853f8b16a980bb22247bde46efd3b148b9
Blake Scott <blakerscott@gmail.com>
2016-03-17 16:45:03 -0700

    remove zip code from the format of the search output

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + zipCode + '&proximity_square=5&stolen_after=1451610062', function(response) {
       console.log(response);
       for(var i = 0; i < response.bikes.length; i++) {
-        $('.showBikes').append("The bikes in " + zipCode + " in " + response.bikes[i].title);
+        $('.showBikes').append(response.bikes[i].title);
         $('.showBikes').append('<ul><li>'+ response.bikes[i].stolen_location + '</li></ul>');
         $('#address').val("'" + response.bikes[i].stolen_location + "'");
         Address();
